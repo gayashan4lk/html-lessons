f91901891e6ada64bf3d46dd7f625fae71afdb0a
Gayashan Wagachchi <gayashan4lk@gmail.com>
2013-12-21 12:00:01 -0400

    javascript first lesson

---
+++ javascript/home.js
@@ -0,0 +1,17 @@
+alert("Only if your above 18, see this page.Adult content included!");
+
+if(navigator.cookieEnabled){
+	document.write("this browser support cookies");
+}
+
+else
+	document.write("this browser does not support cookies<br><br>");\
+	
+	
+function dev(x,y){
+	devision = x/y;
+	return devision;
+}
+	
+	
+
